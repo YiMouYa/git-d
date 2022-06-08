5c46283785b5f3a01705c69854ed908831eb43d8
Ace <lxx18473975688@163.com>
2022-06-08 19:55:04 +0800

    第一次修改克隆下来的工程

--- day20-git-demo/src/com/offcn/demos/Demo01.java
+++ day20-git-demo/src/com/offcn/demos/Demo01.java
@@ -2,7 +2,10 @@ package com.offcn.demos;
 
 public class Demo01 {
     public static void main(String[] args) {
+
+
         System.out.println("aaaaaa");
+        System.out.println("bbbbb");
     }
 
 }
---
+++ day20-git-demo/src/com/offcn/demos/Demo02.java
@@ -0,0 +1,14 @@
+package com.offcn.demos;
+
+public class Demo02 {
+    /**
+     * 新文件为红色，是在工作区的文件
+     * 添加在暂存区的文件是绿色的，对暂存区的文件做了修改，无需重新添加暂存区
+     * 将已经提交到本地库的代码修改，文件会变蓝色
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("新编辑的代码");
+    }
+
+}
